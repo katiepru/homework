d22fa17e7931e3e52269b1f8459e210afe43cb1c
Kaitlin Poskaitis <katiepru@nbcs.rutgers.edu>
2013-01-31 21:12:34 -0500

    avl tree

--- 211/pa1/wordstat.h
+++ 211/pa1/wordstat.h
@@ -1,8 +1,24 @@
+typedef struct VarNode
+{
+	char* word;
+	VarNode *next;
+} VarNode;
+
 typedef struct Node
 {
 	char *word;
 	int count;
 	int num_vars;
-	struct Node *variations;
-	struct Node *next;
+	int balance_factor;
+	struct VarNode *variations;
+	struct Node *left;
+	struct Node *right;
+	struct Node *parent;
 } Node;
+
+typedef struct AVLTree
+{
+	Node *head;
+	int height;
+	int size;
+} AVLTree;
