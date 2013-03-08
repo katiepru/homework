cc72a21a0397b27454e06f7cba90c1fdee9918be
katiepru <katiepru@nbcs.rutgers.edu>
2013-03-08 10:53:27 -0500

    fixing optimizer for communitivity

--- 314/project1/Optimizer.c
+++ 314/project1/Optimizer.c
@@ -34,39 +34,52 @@ int main()
 			if(instr2 != NULL && instr2->opcode == LOADI)
 			{
 				instr3 = instr2->next;
-				if(instr3->opcode == ADD)
+				/*Check to make sure registers match*/
+				if((instr3->field2 == instr1->field1 || instr3->field2 == 
+					instr2->field1) && (instr3->field2 == instr1->field1 || 
+					instr3->field3 == instr2->field1))
 				{
-					opt_calc = instr1->field2 + instr2->field2;
-				}
-				else if(instr3->opcode == SUB)
-				{
-					opt_calc = instr1->field2 - instr2->field2;
-				}
-				else if(instr3->opcode == MUL)
-				{
-					opt_calc = instr1->field2 * instr2->field2;
-				}
-				else
-				{
+					switch(instr3->opcode)
+					{
+					case ADD:
+						opt_calc = instr1->field2 + instr2->field2;
+						break;
+					case SUB:
+						if(instr3->field2 == instr1->field2)
+						{
+							opt_calc = instr2->field2 - instr1->field2;
+						}
+						else
+						{
+							opt_calc = instr1->field2 - instr2->field2;
+						}
+						break;
+					case MUL:
+						opt_calc = instr1->field2 * instr2->field2;
+						break;
+					default:
+						continue;
+					}
+	
+					/*Get Register*/
+					opt_flag = instr3->field1;
+	
+					/*Modify instr3 to contain new instr*/
+					instr3->opcode = LOADI;
+					instr3->field1 = opt_flag;
+					instr3->field2 = opt_calc;
+					instr3->field3 = 0xFFFFF;
+	
+					/*Fix pointers*/
+					instr3->prev = instr1->prev;
+					instr1->prev->next = instr3;
+
+					/*Delete instr1 and instr2*/
+					free(instr1);
+					free(instr2);
+					instr1 = instr3;
 					continue;
 				}
-
-				/*Get Register*/
-				opt_flag = instr3->field3;
-
-				/*Modify instr3 to contain new instr*/
-				instr3->opcode = LOADI;
-				instr3->field1 = opt_flag;
-				instr3->field2 = opt_calc;
-				instr3->field3 = 0xFFFFF;
-
-				/*Fix pointers*/
-				instr3->prev = instr1->prev;
-				instr1->prev->next = instr3;
-
-				/*Delete instr1 and instr2*/
-				free(instr1);
-				free(instr2);
 			}
 		}
 		instr1 = instr1->next;
