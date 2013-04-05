56cbfc0c6471f77726ea15458cb6d5e08172251c
katiepru <katiepru@nbcs.rutgers.edu>
2013-04-05 11:32:00 -0400

    getting there

--- 211/pa3/mystery/mystery.c
+++ 211/pa3/mystery/mystery.c
@@ -1,26 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fib(int n);
+int compute_fib(int n);
 
 int main(int argc, char *argv[])
 {
+	int cache[47];
+	int i;
 	int num;
 
-	if(argc != 2)
+	if(argc <= 2)
 	{
-		fprintf(stderr, "Wrong number of args\n");
-		return 2;
+		if(argc == 1)
+		{
+			fprintf(stderr, 
+				"Error. no argument provided! Syntax: ./myster >pos_int>\n");
+			return 0;
+		}
+	}
+	else
+	{
+		fprintf(stderr, 
+			"ERROR. Too many arguments! Syntax: ./mystery <pos_int>\n");
+		return 0;
 	}
 
-	num = fib(atoi(argv[1]));
+	if(!num)
+	{
+		fprintf(stderr, 
+			"ERROR. Negative Integer! Syntax: ./mystery <pos_int>\n");
+		return 0;
+	}
 
-	printf("%d\n", num);
+	if(num > 46)
+	{
+		fprintf(stderr,
+			"ERROR. Integer too large! Syntax: ./mystery <pos_int>\n");
+	}
+
+	for(i = 0; i < 47; i++)
+	{
+		cache[i] = -1;
+	}
+
+	num = compute_fib(num);
+
+	printf("Value: %d\n", num);
+
 
 	return 0;
 }
 
-int fib(int n)
+int compute_fib(int n)
 {
 
 }
