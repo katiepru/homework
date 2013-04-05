04741e18258cac9953430259e03fc5c74bf0ece3
katiepru <katiepru@nbcs.rutgers.edu>
2013-04-05 11:11:33 -0400

    maybe done with formula

--- 211/pa3/formula/formula.c
+++ 211/pa3/formula/formula.c
@@ -2,6 +2,8 @@
 
 int main(int argc, char *argv[])
 {
+	int num;
+
 	if(argc < 2)
 	{
 		fprintf(stderr, "ERROR: Too few arguments.\n");
@@ -10,7 +12,7 @@ int main(int argc, char *argv[])
 	}
 	if(argc > 2)
 	{
-		fprintf(stderr, "ERROR: Too many arguments.\n")
+		fprintf(stderr, "ERROR: Too many arguments.\n");
 		print_help();
 		return 1;
 	}
@@ -20,9 +22,74 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
+	num = atoi(argv[1]);
+	if(num < 0)
+	{
+		fprintf(stderr, "ERROR: Negative integer detected.\n");
+		print_help();
+		return 2;
+	}
+
+	binomial(num);
+
+	return 0;
+}
+
+void binomial(int num)
+{
+	char **terms = calloc(num+1, sizeof(char *));
+	char pow[11];
+	char str[25];
+	char *ptr;
+	int ncr;
+	int i;
+
+	terms[0] = "1";
+
+	for(i = 1; i < num+1; i++)
+	{
+		ncr = nCr(num, i);
+		sprintf(str, "%d", nCr);
+		sprintf(pow, "%d", i);
+		if(i == 1)
+		{
+			strcat(str, "*x");
+		}
+		else
+		{
+			strcat(str, "*x^");
+			strcat(str, pow);
+		}
+		
+		/*Insert into array*/
+		*ptr = str;
+		terms[i] = strdup(ptr);
+
+		/*Reset strings*/
+		memset(pow, 0, strlen(pow));
+		memset(str, 0, strlen(str));
+	}
+
+	for(i = 0; i < num; i++)
+	{
+		printf("%s + ", terms[i]); 
+	}
+	printf("%s\n", terms[i]);
+	free(terms);
 }
 
 void print_help()
 {
 	printf("Usage: formula <positive integer>\n");
+}
+
+char *strdup(const char *str)
+{
+    int n = strlen(str) + 1;
+    char *dup = malloc(n);
+    if(dup)
+    {
+        strcpy(dup, str);
+    }
+    return dup;
 }
---
+++ 211/pa3/formula/formula.h
@@ -0,0 +1,13 @@
+#ifndef _FORMULA_H
+#define _FORMULA_H
+
+#include "nCr.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *strdup(char *num);
+void binomial(int num);
+void print_help();
+
+#endif
