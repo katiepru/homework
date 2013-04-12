1e2be362a23573cb27e1343e50c3e26ee255075a
katiepru <katiepru@nbcs.rutgers.edu>
2013-04-12 10:22:36 -0400

    starting projecct

--- 211/pa4/y86emul.c
+++ 211/pa4/y86emul.c
@@ -1,4 +1,17 @@
+#include "y86emul.h"
+
 int main(int argc, char *argv[])
 {
+	if(argc != 2)
+	{
+		fprintf(stderr, "ERROR: Wrong number of arguments.\n");
+		print_help();
+		return 1;
+	}
 	return 0;
+}
+
+void print_help()
+{
+	printf("Usage: ./y86emul [-h] <y86 input file>\n");
 }
---
+++ 211/pa4/y86emul.h
@@ -0,0 +1,10 @@
+#ifndef _Y86EMUL_H
+#define Y86_EMUL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void print_help();
+
+#endif
