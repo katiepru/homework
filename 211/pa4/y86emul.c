aa488fb70bb4dd4bce4bfb52497ffdd057febe9d
katiepru <katiepru@nbcs.rutgers.edu>
2013-04-12 11:09:54 -0400

    progress

--- 211/pa4/y86emul.c
+++ 211/pa4/y86emul.c
@@ -2,13 +2,41 @@
 
 int main(int argc, char *argv[])
 {
+	FILE *file;
+
+	/*Check for right number of args*/
 	if(argc != 2)
 	{
 		fprintf(stderr, "ERROR: Wrong number of arguments.\n");
 		print_help();
 		return 1;
 	}
+
+	/*Check for help mode*/
+	if(strcmp(argv[1], "-h") == 0)
+	{
+		print_help();
+		return 0;
+	}
+
+	/*Open the file*/
+	file = fopen(argv[1], "r");
+
+	/*Check if file was found*/
+	if(file == 0)
+	{
+		fprintf(stderr, "ERROR: File %s not found.\n", argv[1]);
+		return 2;
+	}
+
+	run_program(file);
+
 	return 0;
+}
+
+void run_program(FILE *file)
+{
+
 }
 
 void print_help()
--- 211/pa4/y86emul.h
+++ 211/pa4/y86emul.h
@@ -3,8 +3,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-
+void run_program(FILE *file);
 void print_help();
 
 #endif
