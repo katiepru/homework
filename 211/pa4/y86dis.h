5192e2368890e8b0263a2d996136e6436aade8c9
Josh Matthews <josh@jmatthews.us>
2013-05-02 17:17:49 -0400

    Jump locations now human readable.

--- 211/pa4/y86dis.h
+++ 211/pa4/y86dis.h
@@ -6,8 +6,12 @@
 #include <string.h>
 #endif
 
-void disassemble(FILE *file, int* addr_list);
+void disassemble(FILE *file, int* function_list, int* jump_list);
 const char *get_reg(char c);
 void print_help();
 
-void find_functions(FILE *file, int* addr_list);
+void find_functions_and_jumps(FILE *file, int* function_list, int* jump_list);
+
+int check_for_jump(int addr, int* jump_list);
+
+int to_big_endian_int(char* little_endy_str);
