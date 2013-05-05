dc940fbdd07972ffd3cd1e74c3f80f87ec6cc7e9
Josh Matthews <josh@jmatthews.us>
2013-05-05 17:45:26 -0400

    Fixing assembler. Jump locations now work.

--- 211/pa4/y86ass.c
+++ 211/pa4/y86ass.c
@@ -51,7 +51,7 @@ void pre_assemble(FILE *file, int* function_list, int* jump_list)
 {
 	int tmp;
 	unsigned int offset;
-	unsigned int current_position;
+	unsigned int current_position = 0;
 	char val[12];
 	char line[1000];
 	char directive[12];
@@ -87,6 +87,8 @@ void pre_assemble(FILE *file, int* function_list, int* jump_list)
 			current_position += 1;
 		}
 		else if(!strcmp(directive, "rrmovl") ||
+				!strcmp(directive, "pushl") ||
+				!strcmp(directive, "popl") ||
 				!strcmp(directive, "addl") ||
 				!strcmp(directive, "subl") ||
 				!strcmp(directive, "andl") ||
@@ -104,7 +106,7 @@ void pre_assemble(FILE *file, int* function_list, int* jump_list)
 				!strncmp(directive, "read", 4) ||
 				!strncmp(directive, "write", 5))
 		{
-			current_position += 5;
+			current_position += 6;
 		}
 	}
 	printf(".text\t%x\t", offset);
@@ -122,8 +124,8 @@ void assemble(FILE *file, int* function_list, int* jump_list)
 	{
 		sscanf(line, "%s %s %s", directive, arg1, arg2);
 
-		fprintf(stderr, "\n\nline: %s\n", line);
-		fprintf(stderr, "directive: %s\narg1: %s\narg2: %s\n", directive, arg1, arg2);
+		/*fprintf(stderr, "\n\nline: %s\n", line);*/
+		/*fprintf(stderr, "directive: %s\narg1: %s\narg2: %s\n", directive, arg1, arg2);*/
 
 		if(!strcmp(directive, "noop"))
 		{
@@ -135,21 +137,21 @@ void assemble(FILE *file, int* function_list, int* jump_list)
 		}
 		else if(!strcmp(directive,"rrmovl"))
 		{
-			fprintf(stderr, "rrmovl\n");
+			/*fprintf(stderr, "rrmovl\n");*/
 			printf("20");
 			printf("%c", get_register(arg1));
 			printf("%c", get_register(arg2));
 		}
 		else if(!strcmp(directive,"irmovl"))
 		{
-			fprintf(stderr, "irmovl\n");
+			/*fprintf(stderr, "irmovl\n");*/
 			printf("30f");
 			printf("%c", get_register(arg2));
 			printf("%s", &arg1[2]);
 		}
 		else if(!strcmp(directive,"rmmovl"))
 		{
-			fprintf(stderr, "rmmovl\n");
+			/*fprintf(stderr, "rmmovl\n");*/
 			printf("40");
 			printf("%c", get_register(arg1));
 			printf("%c", get_register(&arg2[11]));
@@ -157,7 +159,7 @@ void assemble(FILE *file, int* function_list, int* jump_list)
 		}
 		else if(!strcmp(directive,"mrmovl"))
 		{
-			fprintf(stderr, "mrmovl\n");
+			/*fprintf(stderr, "mrmovl\n");*/
 			printf("50");
 			printf("%c", get_register(arg2));
 			printf("%c", get_register(&arg1[11]));
@@ -176,7 +178,7 @@ void assemble(FILE *file, int* function_list, int* jump_list)
 			else if ( !strcmp ( directive, "xorl" ) ) { printf ( "%c", '3' ) ; }
 			else if ( !strcmp ( directive, "mull" ) ) { printf ( "%c", '4' ) ; }
 
-			fprintf(stderr, "op1\n");
+			/*fprintf(stderr, "op1\n");*/
 			printf("%c", get_register(arg1));
 			printf("%c", get_register(arg2));
 		}
@@ -190,8 +192,8 @@ void assemble(FILE *file, int* function_list, int* jump_list)
 			else if ( !strcmp ( directive, "jne" )  ) { printf ( "%c", '4' ) ;}
 			else if ( !strcmp ( directive, "jge" )  ) { printf ( "%c", '5' ) ;}
 			tmp = atoi(&arg1[2]);
-			printf("%x", jump_list[tmp]);
-			/*print_as_little_endian(jump_list[tmp]);*/
+			/*printf("%x", jump_list[tmp]);*/
+			print_as_little_endian(jump_list[tmp]);
 		}
 		else if(!strcmp(directive, "call"))
 		{
@@ -206,19 +208,19 @@ void assemble(FILE *file, int* function_list, int* jump_list)
 		}
 		else if(!strcmp(directive, "pushl"))
 		{
-			fprintf(stderr, "pushl\n");
+			/*fprintf(stderr, "pushl\n");*/
 			printf("a0");
 			printf("%cf", get_register(arg1));
 		}
 		else if(!strcmp(directive, "popl"))
 		{
-			fprintf(stderr, "popl\n");
+			/*fprintf(stderr, "popl\n");*/
 			printf("b0");
 			printf("%cf", get_register(arg1));
 		}
 		else if(!strncmp(directive, "read", 4))
 		{
-			fprintf(stderr, "read\n");
+			/*fprintf(stderr, "read\n");*/
 			printf("%c", 'c');
 			if(directive[4] == 'b'){ printf("%c", '0'); }
 			else                   { printf("%c", '1'); }
@@ -227,7 +229,7 @@ void assemble(FILE *file, int* function_list, int* jump_list)
 		}
 		else if(!strncmp(directive, "write", 5))
 		{
-			fprintf(stderr, "write\n");
+			/*fprintf(stderr, "write\n");*/
 			printf("%c", 'd');
 			if(directive[5] == 'b'){ printf("%c", '0'); }
 			else                   { printf("%c", '1'); }
@@ -246,56 +248,54 @@ void print_help()
 
 char get_register(char* reg)
 {
-	fprintf(stderr, "got %s\n", reg);
+	/*fprintf(stderr, "got %s\n", reg);*/
 	if(!strncmp(reg, "%eax", 4))
 	{
-		fprintf(stderr, "matched 0\n", reg);
+		/*fprintf(stderr, "matched 0\n", reg);*/
 		return '0';
 	}
 	else if(!strncmp(reg, "%ecx", 4))
 	{
-		fprintf(stderr, "matched 1\n", reg);
+		/*fprintf(stderr, "matched 1\n", reg);*/
 		return '1';
 	}
 	else if(!strncmp(reg, "%edx", 4))
 	{
-		fprintf(stderr, "matched 2\n", reg);
+		/*fprintf(stderr, "matched 2\n", reg);*/
 		return '2';
 	}
 	else if(!strncmp(reg, "%ebx", 4))
 	{
-		fprintf(stderr, "matched 3\n", reg);
+		/*fprintf(stderr, "matched 3\n", reg);*/
 		return '3';
 	}
 	else if(!strncmp(reg, "%esp", 4))
 	{
-		fprintf(stderr, "matched 4\n", reg);
+		/*fprintf(stderr, "matched 4\n", reg);*/
 		return '4';
 	}
 	else if(!strncmp(reg, "%ebp", 4))
 	{
-		fprintf(stderr, "matched 5\n", reg);
+		/*fprintf(stderr, "matched 5\n", reg);*/
 		return '5';
 	}
 	else if(!strncmp(reg, "%esi", 4))
 	{
-		fprintf(stderr, "matched 6\n", reg);
+		/*fprintf(stderr, "matched 6\n", reg);*/
 		return '6';
 	}
 	else if(!strncmp(reg, "%edi", 4))
 	{
-		fprintf(stderr, "matched 7\n", reg);
+		/*fprintf(stderr, "matched 7\n", reg);*/
 		return '7';
 	}
-	fprintf(stderr, "missed it\n");
+	/*fprintf(stderr, "missed it\n");*/
 	return '!';
 }
 
 void print_as_little_endian(int num)
 {
-	int i;
-	for(i=0; i<4; i++)
-	{
-		printf("%x", *(&num + i));
-	}
+	int converted;
+	converted = ((num>>24)&0xff) | ((num<<8)&0xff0000) | ((num>>8)&0xff00) | ((num<<24)&0xff000000);
+	printf("%x", converted);
 }
