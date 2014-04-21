6b51bb5ee55e7699d45864450edab7c86a2db1c3
katiepru <katiepru@nbcs.rutgers.edu>
2014-04-20 23:50:15 -0400

    sdzkfhs

---
+++ os/hw3/part2/test.c
@@ -0,0 +1,33 @@
+#include "mypthread.h"
+
+void *func(void *args)
+{
+    printf("I am a function\n");
+    mypthread_yield();
+    printf("Done yieling bitch\n");
+    mypthread_exit(NULL);
+    return NULL;
+}
+
+void *func2(void *args)
+{
+    printf("I'm great\n");
+    mypthread_exit(NULL);
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    mypthread_t *thread1, *thread2;
+    printf("Creatinf first thread\n");
+    mypthread_create(thread1, func, NULL);
+    printf("Creating second thread\n");
+    mypthread_create(thread2, func2, NULL);
+    printf("Joining 1\n");
+    mypthread_join(thread1, NULL);
+    printf("Joining 2\n");
+    mypthread_join(thread2, NULL);
+    printf("In main\n");
+
+    return 0;
+}
