82595afe919e2f6dfc7f71bf3fe0dedd88c50b5a
katiepru <katiepru@nbcs.rutgers.edu>
2014-04-22 18:06:56 -0400

    mutexes w00t

--- os/hw3/part2/test.c
+++ os/hw3/part2/test.c
@@ -1,29 +1,32 @@
 #include "mypthread.h"
 
+mypthread_mutex_t mutex;
+
 void *func(void *args)
 {
-    while(1);
-    printf("I am a function\n");
+    printf("Locking mutex in func\n");
+    mypthread_mutex_lock(&mutex);
+    printf("Yielding from func\n");
     mypthread_yield();
-    printf("Done yieling bitch\n");
+    printf("unlocking mutex in func\n");
+    mypthread_mutex_unlock(&mutex);
     mypthread_exit(NULL);
     return NULL;
 }
 
 void *func2(void *args)
 {
-    while(1)
-    {
-        printf("func2\n");
-        mypthread_yield();
-    }
-    printf("I'm great\n");
+    printf("locking mutex in func2\n");
+    mypthread_mutex_lock(&mutex);
+    printf("Got it, now unlocking in func2\n");
+    mypthread_mutex_unlock(&mutex);
     mypthread_exit(NULL);
     return NULL;
 }
 
 int main(int argc, char *argv[])
 {
+    mypthread_mutex_init(&mutex, NULL);
     mypthread_t thread1, thread2;
     printf("Creatinf first thread\n");
     mypthread_create(&thread1, func, NULL);
@@ -35,6 +38,8 @@ int main(int argc, char *argv[])
 
     printf("Joining %d\n", thread2);
     mypthread_join(thread2, NULL);
+
+    mypthread_mutex_destroy(&mutex);
 
     printf("In main\n");
 
